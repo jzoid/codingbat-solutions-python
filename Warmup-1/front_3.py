0b030b5e039b74ad5498d020a91d8f51e2149179
jzoid <you@example.com>
2018-03-08 14:31:04 +0000

    the rest

---
+++ Warmup-1/front_3.py
@@ -0,0 +1,10 @@
+def front3(str):
+  ## all these work
+  #
+  # if len(str) < 3:
+  #   return str * 3
+  # return str[:3] * 3
+  #
+  # return str * 3 if len(str) < 3 else str[:3] * 3
+  #
+  return str[:3] * 3
---
+++ Warmup-1/front_back.py
@@ -0,0 +1,12 @@
+def front_back(str):
+  # update to use str[-1] for last
+  if len(str) < 2 :
+    return str
+  elif len(str) == 2:
+    return str[1] + str[0]
+  
+  last = str[len(str) -1]
+  mid = str[1:len(str) -1]
+  first = str[0]
+  
+  return last + mid + first
